d8dfc1635f968892bab2c81bdb561bf6e674cdbf
xuehua <xuehua@gmail.com>
2017-01-24 21:21:20 -0800

    Added a sign-out button

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInUIDelegate {
 
+    @IBAction func signOut(_ sender: Any) {
+        //GIDSignIn.sharedInstance().signOut()
+        GIDSignIn.sharedInstance().disconnect()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
     }
 
     override func didReceiveMemoryWarning() {
